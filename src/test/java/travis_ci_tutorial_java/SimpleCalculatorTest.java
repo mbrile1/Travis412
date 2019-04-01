ad4d0e20691f756310683a9046a9390d4b254108
Mariah Briley <mbrile1@studnets.towson.edu>
2019-04-01 11:57:22 -0400

    Multiply and Divide

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -9,11 +9,22 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 0);
+		assertEquals(calc.add(1, 1), 2);
 	}
 	
 	public void testMinus() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(5, 3), 2);
 	}
+	
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(3, 3), 9);
+	}
+	
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(15, 3), 5);
+	}
+
 }
